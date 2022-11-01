48e4dd027ff2c8982cf1b9edfa49f153eddf005b
Kim Chemnitz <mycomputergirl314@gmail.com>
2022-11-01 18:03:01 +0100

    WIP

--- hpf.py
+++ hpf.py
@@ -1521,7 +1521,7 @@ def pi(depth=10000, iters=10, show_iters=True):
 	trt = two * sqrt(two, depth, iters, show_iters)
 	scalar = trt/x_to_the_y(nn, two)
 	
-	# raise CustomException("idk")
+	raise CustomException("idk")
 	
 	i = _Zero.DeepCopy()
 	summated = _Zero.DeepCopy()
@@ -1539,8 +1539,6 @@ def pi(depth=10000, iters=10, show_iters=True):
 	
 	pi = One.__truediv__(scalar * summated, depth)
 	
-	print("q: %s, %s" % (pi, pi.__repr__()))
-	
 	return pi
 
-hpf_pi = pi(1000, 15, True)
+hpf_pi = pi(2500, 25, True)
